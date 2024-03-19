5ac42313ccfbaf838e4e65f020b374b39a1bc1ee
prashant maurya <prashantmaurya1009@gmail.com>
2024-03-19 10:41:45 +0530

    lazy git pluging add

--- lua/plugins/mini_plugins.lua
+++ lua/plugins/mini_plugins.lua
@@ -107,4 +107,18 @@ return {
 			})
 		end,
 	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 }
