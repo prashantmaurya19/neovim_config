9319b1f9d30640247a24566647ddd42cc1ddb284
Prashant Maurya <prashantmaurya1009@gmail.com>
2025-04-03 21:20:45 +0530

    some change in remaps and cmp

--- lua/plugins/cmp_config.lua
+++ lua/plugins/cmp_config.lua
@@ -1,4 +1,6 @@
 return {
+  "https://github.com/onsails/lspkind.nvim.git",
+  "rambhosale/cmp-bootstrap.nvim",
   "neovim/nvim-lspconfig",
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
@@ -24,27 +26,44 @@ return {
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
+      "https://github.com/onsails/lspkind.nvim.git",
       "saadparwaiz1/cmp_luasnip",
+      "rambhosale/cmp-bootstrap.nvim",
     },
     config = function()
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
       vim.opt.pumblend = 0
-      local tailwind_formater = require("tailwindcss-colorizer-cmp").formatter
+      -- local tailwind_formater = require("tailwindcss-colorizer-cmp").formatter
       cmp.setup({
         formatting = {
           fields = { "abbr", "menu", "kind" },
-          format = function(entry, item)
-            item.abbr = vim.PM.text.truncate(item.abbr, 35)
-            if item.menu ~= nil then
-              if vim.PM.g.cmp_menu_icon[item.menu] == nil then
-                item.menu = vim.PM.text.truncate(item.menu, 30)
-              else
-                item.menu = vim.PM.g.cmp_menu_icon[item.menu]
-              end
-            end
-            -- return item
-            return tailwind_formater(entry, item)
-          end,
+          format = lspkind.cmp_format({
+            mode = "symbol", -- show only symbol annotations
+            maxwidth = {
+              -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+              -- can also be a function to dynamically calculate max width such as
+              -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+              menu = 50, -- leading text (labelDetails)
+              abbr = 50, -- actual suggestion item
+              kind = 50, -- actual suggestion item
+            },
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+              -- ...
+              vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[Lua]",
+                buffer = "[BUF]",
+                cmp_bootstrap = "[󰛆]",
+              })[entry.source.name]
+              return vim_item
+            end,
+          }),
         },
         completion = { completeopt = "menu,menuone,noinsert" },
         snippet = {
@@ -103,6 +122,12 @@ return {
           { name = "cmdline" },
         }),
       })
+      -- cmp.setup.filetype("html", {
+      --   sources = cmp.config.sources({
+      --     { name = "cmp_bootstrap" },
+      --     -- other sources
+      --   }),
+      -- })
     end,
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,70 +1,70 @@
 return {
 
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-telescope/telescope-ui-select.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local telescope = require("telescope")
-			local telescopeConfig = require("telescope.config")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-telescope/telescope-ui-select.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local telescopeConfig = require("telescope.config")
 
-			-- Clone the default Telescope configuration
-			local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-			-- I want to search in hidden/dot files.
-			table.insert(vimgrep_arguments, "--hidden")
-			-- I don't want to search in the `.git` directory.
-			table.insert(vimgrep_arguments, "--glob")
-			table.insert(vimgrep_arguments, "!**/.git/*")
-			-- local select_one_or_multi = function(prompt_bufnr)
-			-- 	local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-			-- 	local multi = picker:get_multi_selection()
-			-- 	if not vim.tbl_isempty(multi) then
-			-- 		require("telescope.actions").close(prompt_bufnr)
-			-- 		for _, j in pairs(multi) do
-			-- 			if j.path ~= nil then
-			-- 				vim.cmd(string.format("%s %s", "edit", j.path))
-			-- 			end
-			-- 		end
-			-- 	else
-			-- 		require("telescope.actions").select_default(prompt_bufnr)
-			-- 	end
-			-- end
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      -- local select_one_or_multi = function(prompt_bufnr)
+      -- 	local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+      -- 	local multi = picker:get_multi_selection()
+      -- 	if not vim.tbl_isempty(multi) then
+      -- 		require("telescope.actions").close(prompt_bufnr)
+      -- 		for _, j in pairs(multi) do
+      -- 			if j.path ~= nil then
+      -- 				vim.cmd(string.format("%s %s", "edit", j.path))
+      -- 			end
+      -- 		end
+      -- 	else
+      -- 		require("telescope.actions").select_default(prompt_bufnr)
+      -- 	end
+      -- end
 
-			telescope.setup({
-				defaults = {
-					-- `hidden = true` is not supported in text grep commands.
-					vimgrep_arguments = vimgrep_arguments,
-					path_display = { "truncate" },
-				},
-				file_ignore_patterns = {
-					"node_modules",
-					"build",
-					"dist",
-					"yarn.lock",
-				},
-				pickers = {
-					layout_strategy = "vertical",
-					layout_config = {
-						height = 40,
-						width = 0.6,
-					},
-					current_buffer_fuzzy_find = {
-						previewer = false,
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      telescope.setup({
+        defaults = {
+          -- `hidden = true` is not supported in text grep commands.
+          vimgrep_arguments = vimgrep_arguments,
+          path_display = { "truncate" },
+        },
+        file_ignore_patterns = {
+          "node_modules",
+          "build",
+          "dist",
+          "yarn.lock",
+        },
+        pickers = {
+          layout_strategy = "vertical",
+          layout_config = {
+            height = 40,
+            width = 0.8,
+          },
+          current_buffer_fuzzy_find = {
+            previewer = false,
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              -- even more opts
+            }),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
